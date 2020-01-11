c97e35904c1afd6225f33af96ea842106e2ab419
hatout01 <hatout1@gmail.com>
2020-01-11 12:32:48 -0800

    changed Api

--- public/app.js
+++ public/app.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
 
-// Firebase
+    // Firebase
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyC6LftGH4U7pUjhGWuIaS4jrd9lDErvT4E",
-    authDomain: "project2-e02f7.firebaseapp.com",
-    databaseURL: "https://project2-e02f7.firebaseio.com",
-    projectId: "project2-e02f7",
-    storageBucket: "project2-e02f7.appspot.com",
-    messagingSenderId: "1006019730458",
-    appId: "1:1006019730458:web:f5681ba8c9783db2ad85a3"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "yourApikey",
+        authDomain: "AuthDomain",
+        databaseURL: "databaseUrl",
+        projectId: "project2-e02f7",
+        storageBucket: "project2-e02f7.appspot.com",
+        messagingSenderId: "1006019730458",
+        appId: "1:1006019730458:web:f5681ba8c9783db2ad85a3"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-    
-var storage = firebase.storage();
+
+    var storage = firebase.storage();
 
 });
 
